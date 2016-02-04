346cd3a037f1371e545e9e5c39df820311b5c1d6
masterhilli <martin.hillbrand@gmail.com>
2016-02-04 08:27:41 +0100

    a running example for spreadsheets manipulation

---
+++ src/main/java/AppRunner.java
@@ -0,0 +1,10 @@
+import com.google.gdata.util.ServiceException;
+import java.io.IOException;
+
+public class AppRunner {
+    public static void main(String[] args) throws IOException {
+        //DriveQuickstart.run(args);
+        SheetsQuickStart.run(args);
+    }
+
+}
