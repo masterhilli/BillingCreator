964027f50c71de9d49f83841c851196bbebb3a04
masterhilli <martin.hillbrand@gmail.com>
2016-03-04 07:15:14 +0100

    refactored test class for InternalProjectMatrix

--- src/test/java/work/billing/Export/TestInternalProjectMatrix.java
+++ src/test/java/work/billing/Export/TestInternalProjectMatrix.java
@@ -1,24 +1,33 @@
 package work.billing.Export;
 
 import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import work.billing.Timesheet.TrackedTime;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 /**
  * Created by mhillbrand on 3/4/2016.
  */
 public class TestInternalProjectMatrix {
-
+    private InternalProjectMatrix internalPrjMatrix;
+    private List<TrackedTime> trackedTimes;
     public static final String PROJECT_NAME = "TestName";
+
+    @BeforeClass
+    public void SetUpClass() {
+        trackedTimes = new ArrayList<>();
+    }
+    @Before
+    public void Setup() {
+        internalPrjMatrix = new InternalProjectMatrix(PROJECT_NAME, trackedTimes);
+    }
 
     @Test
     public void PutHeadingToMatrix_WithStartPos5_ReturnsCorrectHeadingOrder() {
-        InternalProjectMatrix internalPrjMatrix = new InternalProjectMatrix(PROJECT_NAME, new ArrayList<TrackedTime>());
-
         int row = 5;
         internalPrjMatrix.putHeadingToMatrix(row);
 
@@ -32,8 +41,6 @@ public class TestInternalProjectMatrix {
 
     @Test
     public void PutHeadingToMatrix_WithStartPos5_ReturnsCorrectColumnSizeOf5() {
-        InternalProjectMatrix internalPrjMatrix = new InternalProjectMatrix(PROJECT_NAME, new ArrayList<TrackedTime>());
-
         int row = 5;
         internalPrjMatrix.putHeadingToMatrix(row);
 
